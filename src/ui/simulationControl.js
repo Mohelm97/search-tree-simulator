8f7f82af678be5ae64e0d2c29bcf074514905cac
mohelm97 <mohelm97@gmail.com>
2024-11-25 18:43:21 +0200

    Change classes to ids in simulation controls, hide/show pause/play

--- src/ui/simulationControl.js
+++ src/ui/simulationControl.js
@@ -6,16 +6,16 @@ export default class SimulationControl {
         this.element.classList.add("simulation-controls");
         this.element.classList.add("hidden");
         this.element.innerHTML = `
-        <button class="control-button play"><i class="icon-play"></i></button>
-        <button class="control-button pause"><i class="icon-pause"></i></button>
-        <button class="control-button stop"><i class="icon-stop"></i></button>
-        <button class="control-button step-forward" style="margin-right: 10px"><i class="icon-next"></i></button>
-        <button class="control-button decrease-speed"><i class="icon-first"></i></button>
+        <button id="play"><i class="icon-play"></i></button>
+        <button id="pause" class="hidden"><i class="icon-pause"></i></button>
+        <button id="stop"><i class="icon-stop"></i></button>
+        <button id="step-forward" style="margin-right: 10px"><i class="icon-next"></i></button>
+        <button id="decrease-speed"><i class="icon-first"></i></button>
         <div class="speed-control">
             <label for="speed-slider">Speed: <span id="speed-value">500ms</span></label>
             <input id="speed-slider" type="range" class="speed-slider" min="100" max="2000" step="100" value="500">
         </div>
-        <button class="control-button increase-speed"><i class="icon-last"></i></button>
+        <button id="increase-speed"><i class="icon-last"></i></button>
         `;
         document.querySelector("#ui-overlay").append(this.element);
         const speedSlider = this.element.querySelector("#speed-slider");
@@ -28,7 +28,7 @@ export default class SimulationControl {
             speedValueDisplay.textContent = simulationSpeed + "ms";
         });
 
-        this.element.querySelector(".increase-speed").addEventListener("click", () => {
+        this.element.querySelector("#increase-speed").addEventListener("click", () => {
             if (simulationSpeed < 2000) {
                 simulationSpeed += 100;
                 speedSlider.value = simulationSpeed;
@@ -36,20 +36,22 @@ export default class SimulationControl {
             }
         });
 
-        this.element.querySelector(".decrease-speed").addEventListener("click", () => {
+        this.element.querySelector("#decrease-speed").addEventListener("click", () => {
             if (simulationSpeed > 100) {
                 simulationSpeed -= 100;
                 speedSlider.value = simulationSpeed;
                 speedValueDisplay.textContent = simulationSpeed + "ms";
             }
         });
-        this.element.querySelector(".step-forward").addEventListener("click", () => {
+        this.element.querySelector("#step-forward").addEventListener("click", () => {
             this.simulator.step();
         });
-        this.element.querySelector(".stop").addEventListener("click", () => {
+        this.element.querySelector("#stop").addEventListener("click", () => {
             this.simulator.reset();
         });
-        this.element.querySelector(".play").addEventListener("click", () => {
+        this.element.querySelector("#play").addEventListener("click", () => {
+            this.element.querySelector("#pause").classList.remove("hidden");
+            this.element.querySelector("#play").classList.add("hidden");
             this.isPlaying = true;
             const playFunction = () => {
                 this.simulator.step();
@@ -57,7 +59,10 @@ export default class SimulationControl {
             };
             this.playTimeout = setTimeout(playFunction, parseInt(speedSlider.value));
         });
-        this.element.querySelector(".pause").addEventListener("click", () => {
+        this.element.querySelector("#pause").addEventListener("click", () => {
+            this.element.querySelector("#play").classList.remove("hidden");
+            this.element.querySelector("#pause").classList.add("hidden");
+            if (this.playTimeout) clearTimeout(this.playTimeout);
             this.isPlaying = false;
         });
         this.playTimeout = undefined;
@@ -78,14 +83,16 @@ export default class SimulationControl {
     setIsFinished(isFinished) {
         this.isFinished = isFinished;
         if (isFinished) {
-            this.element.querySelector(".step-forward").setAttribute("disabled", true);
-            this.element.querySelector(".play").setAttribute("disabled", true);
+            this.element.querySelector("#step-forward").setAttribute("disabled", true);
+            this.element.querySelector("#play").setAttribute("disabled", true);
         } else {
-            this.element.querySelector(".step-forward").removeAttribute("disabled");
-            this.element.querySelector(".play").removeAttribute("disabled");
+            this.element.querySelector("#step-forward").removeAttribute("disabled");
+            this.element.querySelector("#play").removeAttribute("disabled");
         }
         if (this.playTimeout) clearTimeout(this.playTimeout);
         this.isPlaying = false;
+        this.element.querySelector("#play").classList.remove("hidden");
+        this.element.querySelector("#pause").classList.add("hidden");
     }
     show() {
         this.element.classList.remove("hidden");
